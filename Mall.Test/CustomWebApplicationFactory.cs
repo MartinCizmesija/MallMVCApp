958d530b4d7cae74f17cc39a730ee8c099be9724
bbacani <bernard.bacani@gmail.com>
2022-05-21 14:29:48 +0200

    added integration tests

---
+++ Mall.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,49 @@
+﻿namespace Mall.Test
+{
+    using System;
+    using System.Linq;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Mall.Models;
+
+    namespace RazorPagesProject.Tests
+    {
+        #region snippet1
+        public class CustomWebApplicationFactory<TStartup>
+            : WebApplicationFactory<TStartup> where TStartup : class
+        {
+            protected override void ConfigureWebHost(IWebHostBuilder builder)
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType ==
+                            typeof(DbContextOptions<MallDbContext>));
+
+                    services.Remove(descriptor);
+
+                    services.AddDbContext<MallDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    });
+
+                    var sp = services.BuildServiceProvider();
+
+                    using (var scope = sp.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<MallDbContext>();
+                        var logger = scopedServices
+                            .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                        db.Database.EnsureCreated();
+                    }
+                });
+            }
+        }
+        #endregion
+    }
+}
---
+++ Mall.Test/IntegrationTests/BasicTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Mall.Test.IntegrationTests
+{
+    #region snippet1
+    public class BasicTests
+        : IClassFixture<WebApplicationFactory<Mall.Startup>>
+    {
+        private readonly WebApplicationFactory<Mall.Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<Mall.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Stores")]
+        [InlineData("/Products")]
+        [InlineData("/Category")]
+        [InlineData("/Rooms")]
+        [InlineData("/Home/Privacy")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+    #endregion
+}
